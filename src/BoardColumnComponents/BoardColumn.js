864407bd5f0dedcf5c551f1eeb369580f9282c87
Anna Sharuntsova <Anna.Sharuntsova@confirmit.com>
2021-10-06 20:15:51 +0300

    Try to add tasks. Unsuccesfully

--- src/BoardColumnComponents/BoardColumn.js
+++ src/BoardColumnComponents/BoardColumn.js
@@ -1,15 +1,27 @@
 import {BoardTask} from "./BoardTask";
 import {BoardColumnHeader} from "./BoardColumnHeader";
 import {BoardAddTaskButton} from "./BoardAddTaskButton";
+import {ModalWindow} from "../ModalWindowComponents/ModalWindow"
+import React, {useState} from 'react';
 
 export const BoardColumn = (props) => {
-    const tasks = props.tasks;
+    const [tasks, setTasks] = useState(props.tasks);
     const columnTasks = tasks.map(task =>
         <BoardTask
             key={task.id}
             task={task.description}
         />
     );
+    const addTask = (e) => {
+        let newTasks = tasks;
+        newTasks.push(
+            <BoardTask
+                key={10}
+                task="Untitled"
+            />
+        )
+        setTasks(newTasks);
+    }
     return (
         <div className="board__board-column board-column">
             <BoardColumnHeader name={props.name}/>
@@ -18,7 +30,9 @@ export const BoardColumn = (props) => {
                 {columnTasks}
             </div>
 
-            <BoardAddTaskButton/>
+            <BoardAddTaskButton
+                onClick={addTask}
+            />
         </div>
     );
 }
--- src/BoardColumnComponents/BoardTask.js
+++ src/BoardColumnComponents/BoardTask.js
@@ -1,7 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import {ModalWindow} from "../ModalWindowComponents/ModalWindow"
+
 export const BoardTask = (props) => {
     return (
-        <div className="board-column__item board-column__task">
-            {props.task}
-        </div>
+            <div className="board-column__item board-column__task">
+                {props.task}
+            </div>
     );
 }
