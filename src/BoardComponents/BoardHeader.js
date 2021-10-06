e0bcc41839ae37de41266bae26318c45bde2931a
Anna Sharuntsova <Anna.Sharuntsova@confirmit.com>
2021-10-06 20:14:05 +0300

    Add possibility to edit Board header

--- src/BoardComponents/BoardHeader.js
+++ src/BoardComponents/BoardHeader.js
@@ -1,7 +1,39 @@
+import React, {useState, useRef, useEffect} from 'react';
+import {ReactComponent as ReactLogo} from '../edit_icon.svg';
+
 export const BoardHeader = (props) => {
+    const [editable, setEditable] = useState(false);
+    const [focused, setFocus] = useState(false);
+    const headerText = useRef(null);
+
+    const handleEditClick = (e) => {
+        setEditable(true);
+        headerText.current.focus();
+    }
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            setEditable(false);
+        }
+    }
+    const handleBlur = (e) => {
+        setEditable(false);
+    }
+
     return (
         <div className="board__header">
-            <div className="board__name">{props.name}</div>
+            <div ref={headerText}
+                 tabIndex={0}
+                 onKeyDown={handleKeyDown}
+                 onBlur={handleBlur}
+                 contentEditable={editable}
+                 className="board__name">{props.name}
+            </div>
+
+            <div className="board__edit-icon edit-icon">
+                <ReactLogo
+                    onClick={handleEditClick}
+                />
+            </div>
         </div>
     );
 }
